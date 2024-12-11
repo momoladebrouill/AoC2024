853e829ff148bc0dcdca1ca80feb57cdbd6d88df
momoladebrouill <momoladebrouill@gmail.com>
2024-12-11 06:37:42 +0100

    code cammel bon

--- day11.ml
+++ day11.ml
@@ -9,13 +9,48 @@ let soc c s =
     let q = List.filter (fun e -> e <> "") q in
     q
 
+let evolve_one x = 
+    if x == 0 then [1]
+    else if String.length (string_of_int x) mod 2 == 0 then 
+        begin
+            let s = string_of_int x in
+            let len = String.length s in
+            let p1  = String.sub s 0 (len/2) in 
+            let p2  = String.sub s (len/2) (len/2) in 
+            [int_of_string p1;int_of_string p2]
+        end
+    else
+        [x*2024]
+            
+let rec evolve = function 
+    [] -> []
+    | x::q -> 
 
-let solution l = 
-    let elems = soc " " l in
-    List.iter (fun e->Printf.printf "%s" e) elems; 
-    0
+    if x == 0 then 1::evolve q
+    else if String.length (string_of_int x) mod 2 == 0 then 
+        begin
+            let s = string_of_int x in
+            let len = String.length s in
+            let p1  = String.sub s 0 (len/2) in 
+            let p2  = String.sub s (len/2) (len/2) in 
+            (int_of_string p1) :: (int_of_string p2) :: (evolve q)
+        end
+    else
+        (x*2024):: evolve q
+
+
+let solution () = 
+    let l = [773;79858;0;71;213357;2937;1;3998391] in
+    List.iter (fun e->Printf.printf "%d " e) l;
+    let rec iter i =
+        if i == 0 then l
+        else evolve (iter (i-1))
+    in 
+    let res = iter 25 in
+    Printf.printf "\n";
+
+    List.length res
 
 let () =
-    let file = read_lines (open_in "input.txt") in
-    let sol2 = solution file in
+    let sol2 = solution () in
     Printf.printf "Partie 2 : %d\n" sol2
